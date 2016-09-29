f5fbd4487fbad4ced50ce45da0414b22193fb0bd
jair.garcia@sistemasaberes.com <jair.garcia@sistemasaberes.com>
2016-09-28 19:57:02 -0500

    Standalone runner

---
+++ co.gruposaberes.model/src/co/gruposaberes/model/Runner.java
@@ -0,0 +1,45 @@
+package co.gruposaberes.model;
+
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import org.eclipse.emf.ecore.resource.Resource;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+import org.eclipse.emf.common.util.URI;
+import com.google.inject.Injector;
+
+import co.gruposaberes.model.rails.RubyClass;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		new org.eclipse.emf.mwe.utils.StandaloneSetup().setPlatformUri("../");
+		Injector injector = new RailsStandaloneSetup().createInjectorAndDoEMFRegistration();;
+		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+		File[] projectDirectories = (new File("/Users/jairud/rails/saberes")).listFiles(new FilenameFilter() {
+		    public boolean accept(File dir, String name) {
+		        return new File(dir, name).isDirectory();
+		    }
+		});
+		for(File projectDirectory : projectDirectories){
+			System.out.println("PROYECT: "+projectDirectory.getName());
+			File f = new File(projectDirectory.getPath()+"/app/models");
+			File[] matchingFiles = f.listFiles(new FilenameFilter() {
+				public boolean accept(File dir, String name) {
+			        return name.endsWith("rb");
+			    }
+			});
+			for(File file : matchingFiles){
+				System.out.println("FILE: "+file.getPath());
+				Resource resource = resourceSet.getResource(
+					    URI.createURI("file://"+file.getPath()), true);
+				RubyClass model = (RubyClass) resource.getContents().get(0);
+				System.out.println("Class: "+model.getName()+ " heredada de "+model.getSuperType());
+			}
+
+		}
+	}
+}
